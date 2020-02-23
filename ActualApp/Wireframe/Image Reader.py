167d1721657269028e958f2df47e4356007936e6
brianorion <brianplease1@gmail.com>
2020-02-22 22:21:58 -0500

    Fixed the login and the sign up page.

---
+++ ActualApp/Wireframe/Image Reader.py
@@ -0,0 +1,21 @@
+import cv2
+import os
+
+
+image_dict = {}
+
+
+def check_folder(directory: str):
+    for filename in os.listdir(directory):
+        if "." not in filename:  # if a folder
+            check_folder(directory + "//" + filename)
+        else:  # if it isn't a folder
+            if ".png" in filename:
+                img = cv2.imread(directory + "//" + filename)
+                image_dict[directory + f"/{filename}"] = (img.shape[0], img.shape[1])
+
+
+if __name__ == "__main__":
+    directory = "C:\MyFiles\Python Related Files\TSA\TSA-Software-App-2020-\ActualApp\Wireframe\Image"
+    check_folder(directory)
+    print(image_dict)
